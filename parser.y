87b769244c8e5b9ec5be72d4993616b86be481c1
tanmay_kulkarni <tanmaygk97@gmail.com>
2019-03-21 09:40:21 +0530

    Added a script to speed up testing

--- parser.y
+++ parser.y
@@ -1,9 +1,8 @@
 %{
 	#include <stdio.h>	
 %}
+%token GET FROM WHERE AND OR UPDATE RECORD IN SET TO DELETE 
 
 %%
-
-
 
 %%
---
+++ run.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "!!!WARNING!!! We are using lexer.l and parser.y as the lexer and parser respectively"
+echo ""
+
+echo "Running Bison"
+yacc -d parser.y
+echo ""
+
+echo "Running Flex"
+lex lexer.l
+ehco ""
+
+echo "Compiling them together"
+gcc y.tab.c lex.yy.c -ly -ll
